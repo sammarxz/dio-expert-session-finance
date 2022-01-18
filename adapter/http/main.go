be56d83161c75f8484079758bfd0753c2a21d25c
Sam Marxz <sammarxzz@gmail.com>
2022-01-18 15:55:08 -0300

    Create Health Endpoint;

--- adapter/http/main.go
+++ adapter/http/main.go
@@ -3,12 +3,15 @@ package http
 import (
 	"net/http"
 
+	"github.com/sammarxz/dio-expert-session-finance/adapter/http/actuator"
 	"github.com/sammarxz/dio-expert-session-finance/adapter/http/transaction"
 )
 
 func Init() {
 	http.HandleFunc("/transactions", transaction.GetTransactions)
 	http.HandleFunc("/transactions/create", transaction.CreateTransaction)
+
+	http.HandleFunc("/health", actuator.Health)
 
 	http.ListenAndServe(":5000", nil)
 }
